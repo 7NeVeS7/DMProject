6590d2e5c2a871bd613908aae11ed673c1997112
7NeVeS7 <apaulus@edu.cdv.pl>
2020-01-29 17:48:47 +0100

    add project files

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+
+    public float restartDelay = 1f;
+
+    public void EndGame ()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("game")
+            Invoke("restart", r)
+        }
+    }
+}
